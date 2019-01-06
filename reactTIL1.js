9cf7b17bf94839a99fbfba3b77b5b82a5929cb64
Groundbear <bluenada08@gmail.com>
2019-01-06 21:45:16 +0900

    HelloWorld

---
+++ reactTIL1.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const svr=http.createServer(handler);
+svr.listen(8088)
+
+function handler(req,res) {
+    console.log('url:', req.url)
+    console.log('method:', req.method)
+
+    res.writeHead(200,{'Content-Type' : 'text/html'})
+    res.end('<h1>Hello,World!</h1> \n')
+
+}
